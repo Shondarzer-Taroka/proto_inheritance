ced521d8eda3ce732607d9a963b7617d303504e3
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-20 18:53:08 +0600

    prototype in constructor

--- index.js
+++ index.js
@@ -26,3 +26,21 @@ Object.setPrototypeOf(hen,animal)
 hen.eat()
 dog.eat()
 
+//  // // in constructor
+
+function Animal(name) {
+    this.name=name
+}
+
+
+Animal.prototype.speak=function() {
+    console.log(this.name+' sounds');
+    
+}
+
+
+let dog2=new Animal('dog')
+
+dog2.speak()
+
+console.log(dog2);
