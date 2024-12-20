2f7b4940b8727332410e916e5ddd969274b0c008
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-12-20 18:44:47 +0600

    defined objects

---
+++ index.js
@@ -0,0 +1,22 @@
+let animal = {
+    eat() {
+        console.log('eating');
+
+    }
+}
+
+
+let dog = {
+    bark() {
+        console.log('barking');
+
+    }
+}
+
+let hen = {
+    walking: function () {
+        console.log('walking');
+
+    }
+}
+
